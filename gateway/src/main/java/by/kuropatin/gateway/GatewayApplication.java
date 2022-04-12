148a55512bd916fb0423ca58d5de0bc1f7de8ba5
Pavel Puropatin <kuropatin.pv@gmail.com>
2022-04-13 01:01:19 +0300

    Gateway implementation

---
+++ gateway/src/main/java/by/kuropatin/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package by.kuropatin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(scanBasePackages = "by.kuropatin.gateway")
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
